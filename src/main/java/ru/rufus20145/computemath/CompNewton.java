ba6168358306863f062f22bab4708fd01073e23e
LAPTOP20145 <ivan20027749@gmail.com>
2022-12-21 13:31:06 +0300

    ДЗ_3 (заготовка)

---
+++ src/main/java/ru/rufus20145/computemath/CompNewton.java
@@ -0,0 +1,5 @@
+package ru.rufus20145.computemath;
+
+public interface CompNewton {
+    double calculate(double x, double y, double a, double alpha, double betta);
+}
---
+++ src/main/java/ru/rufus20145/computemath/hw3/Main.java
@@ -0,0 +1,14 @@
+package ru.rufus20145.computemath.hw3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final double ACCURACY = 1e-12;
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+    }
+}
