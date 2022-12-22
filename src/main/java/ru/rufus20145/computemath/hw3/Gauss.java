e7de4dc92980a317c3d99bbc83eef541a35a3b81
LAPTOP20145 <ivan20027749@gmail.com>
2022-12-22 19:25:04 +0300

    ДЗ_3 (начало работы)

---
+++ src/main/java/ru/rufus20145/computemath/hw3/Gauss.java
@@ -0,0 +1,19 @@
+package ru.rufus20145.computemath.hw3;
+
+/**
+ * Класс, реализующий решение СЛАУ методом Гаусса
+ */
+public class Gauss {
+
+    /**
+     * Решение СЛАУ методом Гаусса
+     * 
+     * @param problem  - СЛАУ
+     * @param accuracy - требуемая точность
+     * @return вывод решения в консоль
+     */
+    public static void run(double[][] problem, double accuracy) {
+
+    }
+
+}
--- src/main/java/ru/rufus20145/computemath/hw3/Main.java
+++ src/main/java/ru/rufus20145/computemath/hw3/Main.java
@@ -1,14 +1,34 @@
 package ru.rufus20145.computemath.hw3;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     private static final double ACCURACY = 1e-12;
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        // List<double[][]> problems = new ArrayList<>();
 
+        // problems.add(
+        // new double[][] { { 2, 2, -1, 1, 4 }, { 4, 3, -1, 2, 6 }, { 8, 5, -3, 4, 12 },
+        // { 3, 3, -2, 2, 6 } });
+        // problems.add(
+        // new double[][] { { 1, 7, -9, -8, -7 }, { -3, -18, 23, 28, 5 }, { 0, -3, 6,
+        // -1, 8 },
+        // { -1, -1, 1, 18, -29 } });
+        // problems.add(
+        // new double[][] { { 3, -3, 7, -4, 0 }, { -6, 9, -21, 9, 9 }, { 9, -12, 30,
+        // -22, -2 },
+        // { 6, 0, 6, -31, 37 } });
+        // problems.add(
+        // new double[][] { { 9, -5, -6, 3, -8 }, { 1, -7, 1, 0, 38 }, { 3, -4, 9, 0, 47
+        // }, { 6, -1, 9, 8, -8 } });
+        // problems.add(
+        // new double[][] { { -6, -5, -3, -8, 101 }, { 5, -1, -5, -4, 51 }, { -6, 0, 5,
+        // 5, -53 },
+        // { -7, -2, 8, 5, -63 } });
+
+        // for (int i = 0; i < problems.size(); i++) {
+        // System.out.println("Уравнение " + (i + 1));
+        // Gauss.run(problems.get(i), ACCURACY);
+        // }
     }
 }
