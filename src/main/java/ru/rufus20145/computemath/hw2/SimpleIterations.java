9aee44926b1432151f1b117944f43ecb502ea2ec
LAPTOP20145 <ivan20027749@gmail.com>
2022-12-22 13:29:05 +0300

    Добавил Javadoc в МПИ

--- src/main/java/ru/rufus20145/computemath/hw2/SimpleIterations.java
+++ src/main/java/ru/rufus20145/computemath/hw2/SimpleIterations.java
@@ -9,6 +9,12 @@ public class SimpleIterations {
     private SimpleIterations() {
     }
 
+    /**
+     * Метод для нахождения корней уравнений F(x) = 0 из списка
+     * 
+     * @param problems    - список уравнений в виде F(x) = 0
+     * @param derProblems - список производных уравненийы
+     */
     static void simpleIterationsForList(List<Computable> problems, List<Computable> derProblems) {
         for (int i = 0; i < problems.size(); i++) {
             System.out.println("Equation " + (i + 1) + ":");
@@ -17,23 +23,40 @@ public class SimpleIterations {
         }
     }
 
+    /**
+     * Метод для нахождения корней уравнения F(x) = 0 на отрезке [-100, 100] с
+     * коэффициентом уменьшения шага 0.01
+     * 
+     * @param problem    - уравнение в виде F(x) = 0
+     * @param derProblem - производная уравнения
+     */
     static void localizeRoots(Computable problem, Computable derProblem) {
         int numberOfSolution = 1;
         for (double i = Main.MIN_VALUE; i < Main.MAX_VALUE; i += Main.STEP) {
             if (problem.compute(i) == 0 || problem.compute(i) * problem.compute(i + Main.STEP) < 0) {
                 System.out.printf("%nSolution №%d: %.12f%n%n", numberOfSolution++,
-                        simpleIterations(problem, i, 1 / derProblem.compute(i)));
+                        simpleIterations(problem, i, 1 / derProblem.compute(i) > 0 ? 0.01 : -0.01));// передаем
+                                                                                                    // коэффициент
+                                                                                                    // уменьшения шагаы
             }
         }
     }
 
-    static double simpleIterations(Computable problem, double x, double lambda) {
+    /**
+     * Метод простых итераций для нахождения корня уравнения F(x) = 0
+     * 
+     * @param problem - уравнение в виде F(x) = 0
+     * @param x       - начальное приближение
+     * @param alpha   - коэффициент уменьшения шага
+     * @return - корень уравнения
+     */
+    static double simpleIterations(Computable problem, double x, double alpha) {
         double x0;
         int n = 1;
         do {
             System.out.println("Iteration: " + n++);
             x0 = x;
-            x = x0 - lambda * problem.compute(x0);
+            x = x0 - alpha * problem.compute(x0);
             System.out.printf("x = %.12f%n", x);
         } while (Math.abs(x - x0) > Main.ACCURACY);
         return x;
